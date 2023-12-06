2672db2171b4c03e135134b5340773cbe49f50a7
sheisuka <xxtornex@gmail.com>
2023-12-06 20:59:24 +0700

    :fire:

---
+++ Semester3/LAB5/LAB5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LAB5
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var carPark = new TCarPark();
+
+            carPark.LoadFromTxt("/Users/sheisuka/Desktop/cars.txt");
+
+            foreach (var car in carPark.carPark)
+            {
+                Console.WriteLine(car);
+            }
+
+            Console.WriteLine($"{new String('-', 40)}\nМашины требующие ТО");
+            foreach (var car in carPark.GetCarsNeedCI())
+            {
+                Console.WriteLine(car);
+            }
+
+        }
+    }
+}
---
+++ Semester3/LAB5/LAB5/TAuto.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+
+namespace LAB5
+{
+	public class TAuto
+	{
+		private string Brand;
+		private DateTime DateOfIssue;
+		private int EnginePower;
+		private string ImageUrl;
+
+		public TAuto(string brand, DateTime dateOfIssue, int enginePower, string imageUrl)
+		{
+			this.brand = brand;
+			this.dateOfIssue = dateOfIssue;
+			this.enginePower = enginePower;
+			this.imageUrl = imageUrl;
+		}
+
+		public string brand { get => Brand; init => Brand = value; }
+        public DateTime dateOfIssue { get => DateOfIssue; init => DateOfIssue = value; }
+        public int enginePower { get => EnginePower;
+			set
+			{
+				if (value <= 0)
+				{
+					throw new ArgumentException("Мощность двигателя не может быть меньше нуля");
+				}
+				else
+				{
+					EnginePower = value;
+				}
+			}
+		}
+        public string imageUrl { get => ImageUrl; set => ImageUrl = value; }
+
+        public int GetAnnualTax()
+        {
+			return enginePower * 4;
+        }
+
+		public virtual DateTime GetNextCarInspection()
+		{
+			DateTime currentDate = DateTime.Now;
+			int yearsPassed = currentDate.Year - dateOfIssue.Year;
+			int yearsLeft;
+
+			if (yearsPassed <= 7)
+			{
+				yearsLeft = yearsPassed + (2 - (yearsPassed % 2));
+			}
+			else
+			{
+				yearsLeft = yearsPassed + 1;
+			}
+
+			return dateOfIssue.AddYears(yearsLeft);
+		}
+
+		public static bool operator >(TAuto car, TAuto otherCar)
+		{
+			int result = DateTime.Compare(car.GetNextCarInspection(), otherCar.GetNextCarInspection());
+			return result > 0;
+		}
+
+        public static bool operator <(TAuto car, TAuto otherCar)
+        {
+            int result = DateTime.Compare(car.GetNextCarInspection(), otherCar.GetNextCarInspection());
+            return result < 0;
+        }
+
+        public static bool operator ==(TAuto car, TAuto otherCar)
+        {
+            int result = DateTime.Compare(car.GetNextCarInspection(), otherCar.GetNextCarInspection());
+            return result == 0;
+        }
+
+        public static bool operator !=(TAuto car, TAuto otherCar)
+        {
+			return !(car == otherCar);
+        }
+
+        public override string ToString()
+		{
+			string result = $"Информация об автомобиле:\n\tМарка: {this.brand}" +
+							$"\n\tДата выпуска {this.dateOfIssue.ToString("dd.MM.yyyy")}" +
+							$"\n\tМощность двигателя: {this.enginePower} л.с." +
+							$"\n\tСсылка к изображению: {this.imageUrl}";
+			return result;
+        }
+
+    }
+}
+
